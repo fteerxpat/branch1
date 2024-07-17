a0d77a0546d6c03ed78b811df2bd4c8bf3d04eda
fteerxpat <fluketeerapatlearning@gmail.com>
2024-07-17 14:41:19 +0700

    update App.vue and quasar.config in thai-lang

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,17 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อของคุณ *"
+        hint="ชื่อ และ นามสกุล"
         lazy-rules
         :rules="[(val) => (val && val.length > 0) || 'Please type something']"
       />
 
       <q-input
         filled
-        type="number"
-        v-model="age"
-        label="Your age *"
+        type="เลข"
+        v-model="อายุ"
+        label="อายุของคุณ *"
         lazy-rules
         :rules="[
           (val) => (val !== null && val !== '') || 'Please type your age',
